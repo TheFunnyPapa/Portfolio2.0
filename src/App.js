2142867a904d41662b9a2592cd32a240f45c7463
TheFunnyPapa <gabrielragy@gmail.com>
2023-12-21 16:43:04 -0500

    removed v1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // App.js
 import React from 'react';
 import ControlledCarousel from './components/carousel/ControlledCarousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <div className="App">
